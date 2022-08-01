4c2c148fc910b9c3f92239ba2c53eab02b474350
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-08-01 16:46:46 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+/*         let array = [];
+        for (let i = min; i <= max; i++) {
+            array.push(i);
+        }
+        let start = 0;
+        let end = array.length - 1; */
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.min + this.max) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
